f996adff8d8cf68cdd7688ef78b9610c276ce748
EliakinCosta <eliakim170@gmail.com>
2016-11-28 13:33:01 -0300

    Aluno de uma turma

--- cruds/crud_turma/services.py
+++ cruds/crud_turma/services.py
@@ -60,11 +60,16 @@ def add_aula_turma(turma_id):
 
 
 @turma.route('/disciplina_em_andamento/<professor_id>', methods=['GET'])
-def get_aula_professor_now(professor_id):
-    print(datetime.datetime.now())
+def disciplina_em_andamento(professor_id):
     aulas = (db.session.query(models.Aula).filter(User.id == models.Turma.professor_id).
                                        filter(models.Turma.id== models.Aula.turma_id).
                                        filter(User.id == professor_id).
                                        filter(and_(datetime.datetime.now() >= models.Aula.data_inicio_aula,
                                                    datetime.datetime.now() <= models.Aula.data_fim_aula)).all())
     return jsonify(aula=[aula.serialize() for aula in aulas])
+
+
+@turma.route('/alunos_turma/<turma_id>', methods=['GET'])
+def alunos_turma(turma_id):
+    turma = models.Turma.query.get(turma_id)
+    return jsonify(alunos_turma=[dict(id=aluno.id, username=aluno.username) for aluno in turma.alunos])
